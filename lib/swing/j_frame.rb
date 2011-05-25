988a48096b3f03a4720e9428a6beed4083dae51b
arvicco <arvitallian@gmail.com>
2011-05-25 19:52:44 +0400

    JFrame fixed

--- lib/swing/j_frame.rb
+++ lib/swing/j_frame.rb
@@ -4,21 +4,21 @@ class Swing::JFrame
 #    include AttrSetter
 
   attr_setter :layout, :background, :size, :title,
-              :default_close_operation => JFrame::EXIT_ON_CLOSE #DISPOSE_ON_CLOSE, HIDE_ON_CLOSE
+              :default_close_operation => EXIT_ON_CLOSE #DISPOSE_ON_CLOSE, HIDE_ON_CLOSE
 
   def initialize *args
 
     super *args
 
-#    setup opts
+    setup
 
     self.location_relative_to = nil
     self.visible = true
   end
 
   # Method that subclasses should override to set up their contents before
-  # Frame is made visible
-  def setup opts
+  # JFrame is made visible
+  def setup
   end
 
 end
