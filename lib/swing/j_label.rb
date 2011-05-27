2f32829da41372e322d6f995fb9bc8225ca8aaba
arvicco <arvitallian@gmail.com>
2011-05-27 21:01:55 +0400

    JLabel attributes specified

--- lib/swing/j_label.rb
+++ lib/swing/j_label.rb
@@ -1,5 +1,26 @@
 require 'swing'
 
 class Swing::JLabel
+
+  # SwingConstants.XYZ or JLabel.XYZ for constants...
+  #    UI                     LabelUI • • From L&F
+  #    disabledIcon           Icon • • null
+  #    displayedMnemonic      int/char • • KeyEvent.VK_UNDEFINED - both int and char setters
+  #    displayedMnemonicIndex int -1        ?
+  #    horizontalAlignment    int • • LEADING - Also: TRAILING, LEFT, RIGHT, or CENTER
+  #    horizontalTextPosition int • • TRAILING - Also LEFT, RIGHT, TOP, BOTTOM, CENTER
+  #    icon                   Icon • • null
+  #    iconTextGap            int • • 4
+  #    labelFor               Component • • null
+  #    text                   String • • null
+  #    verticalAlignment      int • • CENTER - Also TOP, BOTTOM, or CENTER
+  #    verticalTextPosition   int • • CENTER - Also LEFT, RIGHT, TOP, BOTTOM
+  #    ----Getters only - NO Setters!
+  #    accessibleContext AccessibleContext • JLabel.AccessibleJLabel
+
+  attr_setter :disabled_icon, :displayed_mnemonic, :displayed_mnemonic_index,
+              :horizontal_alignment, :horizontal_text_position, :icon, :icon_text_gap,
+              :label_for, :text, :vertical_alignment, :vertical_text_position
+
 end # class JLabel
 
--- spec/swing/j_label_spec.rb
+++ spec/swing/j_label_spec.rb
@@ -9,5 +9,29 @@ describe Swing::JLabel do
     it_behaves_like 'enhanced Awt::Component'
     it_behaves_like 'enhanced Swing::JComponent'
 
+    it 'allows to set properties via constructor' do
+      field = Swing::JTextField.new
+      icon = Swing::Icon.new
+      disabled_icon = Swing::Icon.new
+      properties = {
+          :disabled_icon => disabled_icon,
+          :displayed_mnemonic => 39,
+          :displayed_mnemonic_index => -1,
+          :horizontal_alignment => Swing::JLabel::RIGHT,
+          :horizontal_text_position => Swing::JLabel::RIGHT,
+          :icon => icon,
+          :icon_text_gap => 13,
+          :label_for => field,
+          :text => 'Bleh',
+          :vertical_alignment => Swing::JLabel::TOP,
+          :vertical_text_position => Swing::JLabel::TOP
+      }
+      @label = described_class.new *(new_args.push properties)
+
+      properties.each do |name, value|
+        @label.send(name).should == value
+      end
+    end
+
   end
 end
